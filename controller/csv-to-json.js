38db28c2354d68e8dae67c81d482d83e6192f4c9
Amilcar34 <alberino-l@hotmail.com>
2019-04-30 01:01:37 -0300

    el json en text area
    el famoso json de los archivos CSV al fin llego al text area como corresponde

--- controller/csv-to-json.js
+++ controller/csv-to-json.js
@@ -6,18 +6,24 @@ var CSVToJSON = function() {
     reader.onload = function(e) {
       var csvText = e.target.result;
       var allTextLines = csvText.split(/\r\n|\n/);
-      jQuery( '#xlx_json' ).val( $.parseJSON(allTextLines) );
       //Split per line on tabs and commas
       var headers = allTextLines[0].split(/\t|,/);
       var lines = [];
       var client = [];
-
+      
       for (var i=1; i<allTextLines.length; i++) {
-          var data = allTextLines[i].split(/\t|,/);
-          if (data.length == headers.length) {
-            var regist = {"creation":data[1], "cp":data[5], "name":data[12], "phone":data[13], "platform":data[11]};
-            client.push(regist);
-          }
+        var data = allTextLines[i].split(/\t|,/);
+        if (data.length == headers.length) {
+          let creation = data[1].substring(data[1].search('-')+2, data[1].search('T')).replace(' ', '').trim(); 
+          let cp = data[5].replace(/"/gi, '').trim();
+          let name = data[12].replace(/"/gi, '').trim();
+          //let phoneMatch = data[13].match(/[0-9]/);
+          let phone =  data[13].replace(/^\D+/g, '').trim();
+          let platform = data[11].replace(/"/gi, '').trim();
+          var regist = {"creation":creation, "cp":cp, "name":name, "phone":phone, "platform":platform};
+          client.push(regist);
+        }
+        jQuery( '#xlx_json' ).val(  JSON.stringify( eval(client) ).replace(/\\u0000/gi,'') );
       }
       console.log(client)
 
